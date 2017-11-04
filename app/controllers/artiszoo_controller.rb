3a1618e9c861da175ed7a25a5e2454e748925ef7
henockbelete <etyeheny@gmail.com>
2017-11-04 16:19:42 +0100

    artiszoo animals

--- app/controllers/artiszoo_controller.rb
+++ app/controllers/artiszoo_controller.rb
@@ -2,5 +2,11 @@ class ArtiszooController < ApplicationController
 
   def index
    @artiszoo = Artiszoo.all
+  end
+
+  def show
+     @artiszoo = Artiszoo.find(params[:id])
+     @animals =@artiszoo.animals
+  end
+
  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "artiszoo#index"
 
-  get "artiszoo" => "artiszoo#index"
+  get "artiszoos" => "artiszoo#index"
+
+  get "artiszoos/:id" => "artiszoo#show", as: :artiszoo
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
