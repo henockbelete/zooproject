cafc04f3131c1e6950a47ed55abac40abddf0541
NEETHU <neethusanthosh01@gmail.com>
2017-11-04 15:46:12 +0100

    edi ,update & delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "animals#index"
 
-# get "animals" =>"animals#index"
-# get "animals/new" => "animals#new", as: :new_animal
-# get "animals/:id" => "animals#show", as: :animal
-# post "animals" => "animals#create"
-
-resources :animals
+ get "animals" =>"animals#index"
+get "animals/new" => "animals#new", as: :new_animal
+ get "animals/:id" => "animals#show", as: :animal
+post "animals" => "animals#create"
+get 'animals/:id/edit' => 'animals#edit', as: :edit_animal
+patch "animals/:id" => "animals#update"
+delete "animals/:id" => "animals#destroy"
+#resources :animals
 end
