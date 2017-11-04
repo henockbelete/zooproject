a8c4e4f35899821ae226bb2409663229ead7ad46
NEETHU <neethusanthosh01@gmail.com>
2017-11-04 15:13:45 +0100

    Create new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "animals#index"
 
-get "animals" =>"animals#index"
-get "animals/:id" => "animals#show", as: :animal
+# get "animals" =>"animals#index"
+# get "animals/new" => "animals#new", as: :new_animal
+# get "animals/:id" => "animals#show", as: :animal
+# post "animals" => "animals#create"
+
+resources :animals
 end
